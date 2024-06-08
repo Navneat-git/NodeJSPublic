38de51caf11676a116269d02ac3308f847672b05
Navneat-git <navneet.bhardwaj40553@gmail.com>
2024-06-08 15:33:41 +0530

    shift MongoDB server to online

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
+
 const bodyParser=require('body-parser');
 app.use(bodyParser.json()); //req.body
 // const fs=require('./filesystem');
 const notes=require('./notes');
 // const ld=require('./lodash');
 const db=require('./db');
+require('dotenv').config();
+const PORT=process.env.PORT || 3000;
+
 const productRoutes=require('./routes/productRoutes');
 app.use('/item',productRoutes);
 
@@ -16,3 +20,8 @@ console.log(age);
 var result=notes.addNumber(age,10);
 
 console.log(result);
+
+
+app.listen(PORT,()=>{
+    console.log("Listening server on 3000 Port");
+});
